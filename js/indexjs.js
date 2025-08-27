974be5d3177e2c85d158700bb299896913aed532
lvfahim <mdfahim26320@gmail.com>
2025-08-28 00:00:00 +0600

    responsive section test push of phone

--- js/indexjs.js
+++ js/indexjs.js
@@ -131,3 +131,28 @@ document.getElementById('copy-btn-rail').addEventListener('click',
     }
 )
 // copyNumber section end
+
+
+// call section start
+function creatInnertext(id){
+    const number=document.getElementById(id);
+    const numberInnertext=number.innerText;
+    const numberInnertextPar=parseInt(numberInnertext);
+    return numberInnertextPar;
+}
+document.getElementById('call-btn-national').addEventListener('click',
+    function (){
+        const textNumber=document.getElementById('copy-text-national').innerText
+        const lostCoinNumber=20;
+        const PresentCoin= creatInnertext('coin-btn');
+        if(PresentCoin <20){
+            alert("You don't have enough coins. A minimum of 20 coins is required to make a call");
+            return;
+        }
+        const remainingCoin=PresentCoin-lostCoinNumber;
+        document.getElementById('coin-btn').innerText=remainingCoin;
+        alert(textNumber)
+    }
+)
+
+// call section end
